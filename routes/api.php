76fe902db87a9d6448d3b449278c4b963b036e5f
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-09-15 21:01:28 -0400

    a

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TestamentoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/testamento', [TestamentoController::class, 'index']);
+Route::get('/testamento/{id}', [TestamentoController::class, 'show']);
+Route::post('/testamento', [TestamentoController::class, 'store']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
